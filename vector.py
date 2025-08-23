1e07f839862eda9061df45cdcd44a037abe95e90
Navaneeth Mohan <navaneethkrishnan24@gmail.com>
2025-08-23 20:56:14 +0530

    modified code to ingest data from releases

--- vector.py
+++ vector.py
@@ -1,52 +1,34 @@
-from typing_extensions import Doc
 from langchain_ollama import OllamaEmbeddings
 from langchain_chroma import Chroma
 from langchain_core.documents import Document
 from ollama import embeddings
-from patch_parser import create_pr_document_from_patch
+from pr_parser import create_documents_from_prs
+from pr_fetcher import get_prs_for_release
 
 import os
 
+def ingest_prs_to_db(owner: str, repo: str, token: str):
 
-embeddings = OllamaEmbeddings(model='mxbai-embed-large')
-db_location = "./chroma_langchain_db"
-directory_path = "./data_diffs"
-documents = []
-ids = []
-add_document = not os.path.exists(db_location)
-
-if add_document:
+    embeddings = OllamaEmbeddings(model='mxbai-embed-large')
+    db_location = f"./chroma_db_{owner}_{repo}"
     
-    for i,filenames in enumerate(os.listdir(directory_path)):
-        full_path = os.path.join(directory_path,filenames)
-        
-        with open(full_path,'r') as file:
-            patch = file.read()
-        
-        pagecontent,metadata = create_pr_document_from_patch(patch=patch)
-        document = Document(page_content=pagecontent,metadata=metadata,id=str(i))
+    documents = []
+    ids = []
+    add_document = not os.path.exists(db_location)
 
-        ids.append(str(i))
-        documents.append(document)
+    if add_document:   
+        pr_set,version = get_prs_for_release(owner, repo, token)
+        documents = create_documents_from_prs(pr_set)
 
-vector_store = Chroma(
-    collection_name = "pull_request_tracker",
-    persist_directory=db_location,
-    embedding_function=embeddings
-)
+    vector_store = Chroma(
+        collection_name = "pull_request_tracker",
+        persist_directory=db_location,
+        embedding_function=embeddings
+    )
 
-if add_document:    
-    vector_store.add_documents(documents=documents, ids=ids)
+    if add_document:    
+        vector_store.add_documents(documents=documents, ids=ids)
+    
+    return vector_store
 
 
-retriever = vector_store.as_retriever(
-    search_kwargs = {"k": 3}
-)
-
-
-if __name__ == "__main__":
-
-    query = "GET RECENT CODE CHANGES" 
-    diff = retriever.invoke(query)
-
-    print(diff[0].page_content)
